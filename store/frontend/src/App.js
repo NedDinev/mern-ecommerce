ee3967e4632bf360bbfd42adeae7fe704b11795a
NedDinev <ned.dinev@gmail.com>
2023-05-28 20:41:30 +0300

    first commit

---
+++ store/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">MERN ecommerce</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
