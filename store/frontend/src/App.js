6027970b670a24cdfbf0398ca56a88daba493c79
NedDinev <ned.dinev@gmail.com>
2023-05-29 15:50:34 +0300

    list products

--- store/frontend/src/App.js
+++ store/frontend/src/App.js
@@ -1,10 +1,38 @@
+import { data } from "./data";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">MERN ecommerce</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img
+                  className="product-image"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>$</strong>
+                  {product.price}
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
