0567a007f4d61aeedd4825fa6e600513626e5934
NedDinev <ned.dinev@gmail.com>
2023-06-02 16:45:33 +0300

    Add Bootstrap

--- store/frontend/src/App.js
+++ store/frontend/src/App.js
@@ -2,20 +2,33 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import Product from "./components/Product/Product";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header className="App-header">
-          <Link to="/">MERN ecommerce</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand as={Link} to="/">
+                MERN ecommerce
+              </Navbar.Brand>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<Product />}></Route>
-            <Route path="/" element={<Home />}></Route>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<Product />}></Route>
+              <Route path="/" element={<Home />}></Route>
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">ALl rights reserved &copy;</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
