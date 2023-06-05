ec342a4dfd7d91fd590e8b91d2d22bdf0d7daa44
NedDinev <ned.dinev@gmail.com>
2023-06-05 18:57:14 +0300

    Dynamically change page title using Helmet

--- store/frontend/src/pages/HomePage.js
+++ store/frontend/src/pages/HomePage.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import logger from "use-reducer-logger";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -43,6 +44,9 @@ export default function Home() {
 
   return (
     <div>
+      <Helmet>
+        <title>MERN ecommerce</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
