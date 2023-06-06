e6c89a4e49007e76a75fb9e6607b3135a069cadf
NedDinev <ned.dinev@gmail.com>
2023-06-06 18:36:19 +0300

    Create getError util to handle error response

--- store/frontend/src/pages/ProductPage.js
+++ store/frontend/src/pages/ProductPage.js
@@ -6,6 +6,7 @@ import Rating from "../components/Rating";
 import { Helmet } from "react-helmet-async";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -39,7 +40,7 @@ export default function Product() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
     };
     fetchData();
